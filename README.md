bd0965d3d4ebf3f13cbeff69418938cf7b0fb2e3
Faiyaz Haider <faiyazhaider@ymail.com>
2014-04-25 08:09:48 -0400

    Refactored

---
+++ README.md
@@ -0,0 +1,3 @@
+#To project#
+
+Shell script to navigate user to project directory, and create short name for easy access (zsh)
--- to_project.plugin.zsh
+++ to_project.plugin.zsh
@@ -1,104 +1,103 @@
-function to_project() 
+function to_project()
 {
 	## Localize
-	typeset -a env domain app
-	typeset invalid_choice web_root
+	typeset invalid_choice web_root a b c small_env small_domain #local variables
+	typeset -i selection #local integer variable
+	typeset -a items item environment domain application project #local arrays 
+	typeset -A chosen_item #associative array
 
-	## Set some intial arrays
-	env=('development' 'test' 'production') # custom environments
-	domain=('faiyazhaider' 'funcfoo' 'devoop') # custom domains
-	app=('custom' 'mobile' 'web') # custom apps
-
-	## Set some common variables
+	## Set the variables
 	invalid_choice="\n${bg[red]}${fg_bold[red]} Invalid choice ${reset_color}"
 	web_root="/Library/WebServer/Documents/www"
 
-	# Start out by changing directory to the root of of web directory
+	## Set the arrays
+	items=('environment' 'domain' 'application')
+	set -A ${items[1]} 'development' 'test' 'production' # custom environments
+	set -A ${items[2]} 'faiyazhaider.com' 'funcfoo.com' 'devoop.com' # custom domains
+	set -A ${items[3]} 'custom' 'mobile' 'web' # custom apps
+
+	## Change into the web directory
 	cd $web_root
-	## Begin user input selection process
-	# 1) Environment
-	clear
-	print "${bg[green]}${fg_bold[green]} Select environment ${reset_color}\n"
-	print "Type ${fg_bold[green]}1${reset_color} for ${fg_bold[green]}${env[1]}${reset_color}"
-	print "Type ${fg_bold[green]}2${reset_color} for ${fg_bold[green]}${env[2]}${reset_color}"
-	print "Type ${fg_bold[green]}3${reset_color} for ${fg_bold[green]}${env[3]}${reset_color}\n"
-	read selection"?Type the number: "
-	# Condition to verify if user selected a valid environment
-	if [[ $selection -gt 0 && $selection -le ${#env[@]} ]]; then
-		chosen_env=${env[$selection]}
-		# Move into the chosen environment
-		cd $chosen_env
-		# Using sub-string to make smaller environment string name
-		if [[ $chosen_env == ${env[1]} ]]; then
-			small_env=${chosen_env:0:3}
-		elif [[ $chosen_env == ${env[3]} ]]; then
-			small_env=${chosen_env:0:4}
-		else 
-			small_env=$chosen_env
-		fi
-		# 2) Domain
-		clear
-		print "${bg[yellow]}${fg_bold[yellow]} Select domain ${reset_color}\n"
-		print "Type ${fg_bold[yellow]}1${reset_color} for ${fg_bold[yellow]}${domain[1]}${reset_color}"
-		print "Type ${fg_bold[yellow]}2${reset_color} for ${fg_bold[yellow]}${domain[2]}${reset_color}"
-		print "Type ${fg_bold[yellow]}3${reset_color} for ${fg_bold[yellow]}${domain[3]}${reset_color}\n"
-		read selection"?Type the number: "
-		# Condition to verify if user selected a valid domain
-		if [[ $selection -gt 0 && $selection -le ${#domain[@]} ]]; then
-			chosen_domain=${domain[$selection]}
-			# Move into the chosen domain
-			cd ${chosen_domain}.com
-			# 3) Application
-			clear
-			print "${bg[cyan]}${fg_bold[cyan]} Select application ${reset_color}\n"
-			print "Type ${fg_bold[cyan]}1${reset_color} for ${fg_bold[cyan]}${app[1]}${reset_color}"
-			print "Type ${fg_bold[cyan]}2${reset_color} for ${fg_bold[cyan]}${app[2]}${reset_color}"
-			print "Type ${fg_bold[cyan]}3${reset_color} for ${fg_bold[cyan]}${app[3]}${reset_color}\n"
-			read selection"?Type the number: "
-			# Condition to verify if user selected a valid application
-			if [[ $selection -gt 0 && $selection -le ${#app[@]} ]]; then
-				chosen_app=${app[$selection]}
-				# Move into the chosen application
-				cd $chosen_app
+	clear # clear screen
+	# Loop through the 'items' array
+	for (( a=1; a<=${#items}; a++ )); do
+		print "\n${bg[green]}${fg_bold[green]} Select $items[$a] ${reset_color}\n"
+		# Based on the value of $a, the 'item' array will hold either env, domain or app
+		item=(${(P)items[$a]}) # parameter expansion flag
+		# Based on the item, loop through its avilable values
+		for (( b=1; b<=${#item}; b++ )); do
+			# Print choices based on returned values
+			print "Type ${fg_bold[green]}${b}${reset_color} for ${fg_bold[green]}${item[$b]}${reset_color}"
+		done
+		print ""
+		# Allow user to input a number and catch the input with 'selection' variable
+		read selection"?Type the number: ${fg_bold[white]}"
+		# Condition to verify if user selected a valid number
+		if [[ $selection -gt 0 && $selection -le ${#item} ]]; then
+			cd ${item[$selection]} # Change directory to selected item
+			chosen_item+=(${items[$a]} ${item[$selection]}) # Save selection in an associative array
+			clear # clear screen
+			# Condition to verify if we are at the last item 
+			if [[ $a -eq ${#items} ]]; then
 				# Loop through the current list of directories
-				for i (*/); do
+				for c (*/); do
 					# Except 'log', put all on a project array
-					if [[ ${i:0:-1} != 'log' ]]; then
+					if [[ ${c:0:-1} != 'log' ]]; then
 						# the -1 deletes the '/' from the end of the string
-						project+=("${i:0:-1}")
+						project+=("${c:0:-1}")
 					fi
 				done
 				# If the project array is not empty, we have projects
-				if [[ ${#project[@]} -gt 0 ]]; then
-					# 4) project
-					clear
-					print "${bg[white]}${fg_bold[white]} Select project ${reset_color}\n"
+				if [[ ${#project} -gt 0 ]]; then
+					print "\n${bg[green]}${fg_bold[green]} Select project ${reset_color}\n"
 					# Loop through the project array to create selection
-					for (( i=1; i<=${#project[@]}; i++ )); do
-						print "Type ${fg_bold[white]}${i}${reset_color} for ${fg_bold[white]}${project[${i}]}${reset_color}"
+					for (( d=1; d<=${#project}; d++ )); do
+						print "Type ${fg_bold[green]}${d}${reset_color} for ${fg_bold[green]}${project[${d}]}${reset_color}"
 					done
-					read selection"?Type the number: "
+					print ""
+					# Allow user to input a number and catch the input with 'selection' variable
+					read selection"?Type the number: ${fg_bold[white]}"
 					# Condition to verify if user selected a valid project
-					if [[ $selection -gt 0 && $selection -lt ${#project[@]} ]]; then
-						chosen_project=${project[$selection]}
-						# Move into the chosen project
-						cd $chosen_project
-						#create a variable to represent the project directory and use 'eval' command to execute
-						eval "${chosen_env}_${chosen_domain}_${chosen_app}_${chosen_project}=$PWD"
+					if [[ $selection -gt 0 && $selection -le ${#project} ]]; then
+						# Change directory to selected project
+						cd ${project[$selection]} 
+						chosen_item+=(project ${project[$selection]}) # Save selection in an associative array
+						clear # Clear screen
+						# Remove the '.com' from end of the domain names
+						small_domain=${chosen_item[domain]:0:-4}
+						# Shorten the environment names 
+						if [[ ${chosen_item[environment]} == 'development' ]]; then
+							small_env=${chosen_item[environment]:0:3}
+						else
+							small_env=${chosen_item[environment]:0:4}
+						fi
+						clear # Clear screen
+						# Output Success, and print final result to user
+						print "\n${fg_bold[green]}Success!!${reset_color}"
+						print "\n${fg_bold[green]}Environment: ${fg_bold[white]}${chosen_item[environment]}${reset_color}"
+						print "${fg_bold[green]}Domain: ${fg_bold[white]}${chosen_item[domain]}${reset_color}"
+						print "${fg_bold[green]}Application: ${fg_bold[white]}${chosen_item[application]}${reset_color}"
+						print "${fg_bold[green]}Total projects: ${fg_bold[white]}${#project}${reset_color}"
+						print "${fg_bold[green]}Selected project: ${fg_bold[white]}${chosen_item[project]}${reset_color}"
+						print "${fg_bold[green]}Access this project with: ${fg_bold[white]}${small_env}_${small_domain}_${chosen_item[application]}_${chosen_item[project]}${reset_color}"
+						# Create shortcut to the project directory
+						eval "${small_env}_${small_domain}_${chosen_item[application]}_${chosen_item[project]}=$PWD"
+					else # User provided incorrect data
+						print $invalid_choice
+						return
 					fi
 				else # The project array is empty
-					print "${fg_bold[red]}Sorry, but you currently have no ${fg_bold[white]}${chosen_app} app ${fg_bold[red]}setup for ${fg_bold[white]}${chosen_domain}.com${reset_color}"
+					# Output result to user
+					print "\n${fg_bold[green]}Environment: ${fg_bold[white]}${chosen_item[environment]}${reset_color}"
+					print "${fg_bold[green]}Domain: ${fg_bold[white]}${chosen_item[domain]}${reset_color}"
+					print "${fg_bold[green]}Application: ${fg_bold[white]}${chosen_item[application]}${reset_color}"
+					print "${fg_bold[green]}Total projects: ${fg_bold[white]}${#project}${reset_color}"
+					print "\n${fg_bold[red]}Sorry, but you currently have no project setup${reset_color}"
 				fi
-			else # User provided an incorrect data for app selection
-				print $invalid_choice
 			fi
-		else # User provided an incorrect data for domain selection
+		else # User provided incorrect data
 			print $invalid_choice
+			return
 		fi
-	else # User provided an incorrect data for env selection
-		print $invalid_choice
-	fi
-
-	# Destroy arrays and variables
-	unset -v project selection small_env chosen_env chosen_domain chosen_app chosen_project
+	done
 }
